143d597b1e5d1eaf631e1b655e3a1d8f75488029
FilipePrates <filipeprates@poli.ufrj.br>
2024-07-23 01:58:05 -0300

    Added clean_log_file to utils.py

--- flows.py
+++ flows.py
@@ -1,7 +1,7 @@
 # o Flow propriamente dito.
 
 from prefect import Flow, Parameter
-
+from utils import clean_log_file
 from tasks import (
     download_new_cgu_terceirizados_data,
     save_raw_data_locally,
@@ -17,11 +17,12 @@ from tasks import (
 # Executar captura e materialização a cada ~4 meses.
 with Flow("Dados Abertos de Terceirizados de Órgãos Federais - Captura") as capture:
     # # SETUP #
+    clean_log_file("flow_logs.txt")
+
     # # EXTRACT #
     rawData = download_new_cgu_terceirizados_data()
     rawFilePaths = save_raw_data_locally(rawData)
     
-    # rawFilePaths = {'rawFilePaths':['cgu_terceirizados_local/year=2024/month=maio.xlsx']}
     # # CLEAN #
     parsedData = parse_data_into_dataframes(rawFilePaths)
     parsedFilePaths = save_parsed_data_as_csv_locally(parsedData)
@@ -32,6 +33,9 @@ with Flow("Dados Abertos de Terceirizados de Órgãos Federais - Captura") as ca
     
 
 with Flow("Dados Abertos de Terceirizados de Órgãos Federais - Materialização (DBT)") as materialize:
+    # # SETUP #
+    clean_log_file("flow_logs.txt")
+
     columns = set_columns_types()
     columns = rename_columns_following_style_manual()
 
